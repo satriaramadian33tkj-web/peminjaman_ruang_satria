ac8832fc3af4557f5f1ad582b69ba776d1f2f55e
satriaramadian33tkj-web <satria_ramadian_33tkj@student.smktelkom-mlg.sch.id>
2026-01-15 12:33:12 +0700

    Update config.php

--- config.php
+++ config.php
@@ -1,15 +1,16 @@
 <?php
-$host = 'localhost';
-$user = 'roselina';
-$pass = 'Roselina@123';
-$db   = 'PEMINJAMAN_RUANG';
+$db_host = 'database-1.ck8jzp2twzg8.us-east-1.rds.amazonaws.com';
+$db_user = 'admin';
+$db_pass = 'admin123';
+$db_name = 'aidandb';
 
-$conn = new mysqli($host, $user, $pass, $db);
+$conn = new mysqli($db_host, $db_user, $db_pass, $db_name);
+
 if ($conn->connect_error) {
     die("Koneksi gagal: " . $conn->connect_error);
 }
 
-// tampilkan error PHP (bisa dimatikan di production)
+// tampilkan error PHP
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
